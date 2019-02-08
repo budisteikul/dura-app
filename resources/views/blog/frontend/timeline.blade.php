c64ef5c655e4b80febab1e4c327bfa07fd6e7ebd
budi <budi@budis-iMac.local>
2019-02-08 23:10:04 +0700

    #1891

--- resources/views/blog/frontend/timeline.blade.php
+++ resources/views/blog/frontend/timeline.blade.php
@@ -20,7 +20,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand page-scroll" href="/">{{ $setting->judul1 }}</a>
+                <a class="navbar-brand page-scroll" href="/">{{ $setting->title1 }}</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
