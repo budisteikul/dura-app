9c67754be687f5ec23eb1a523eb9d4d22b676fb9
budi <budi@budis-iMac.local>
2019-02-24 11:55:40 +0700

    #1891

--- resources/views/blog/frontend/timeline.blade.php
+++ resources/views/blog/frontend/timeline.blade.php
@@ -138,7 +138,7 @@
 						if($d>1)
 						{
 						?>
-								<img id="{{ $attachment->id }}" onClick="openFancyBox('{{ $result->id }}','{{ $index }}','{{ $attachment->id }}','{{ $setting->user_id }}') return false;" class="image-photo" src="{{ asset('/storage/'. $setting->user_id .'/images/250/'. $attachment->file_name) }}" alt=""  />
+								<img id="{{ $attachment->id }}" onClick="openFancyBox('{{ $result->id }}','{{ $index }}','{{ $attachment->id }}','{{ $setting->user_id }}'); return false;" class="image-photo" src="{{ asset('/storage/'. $setting->user_id .'/images/250/'. $attachment->file_name) }}" alt=""  />
 								
 						<?php	
 						}
