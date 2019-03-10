de32655581dcf97a0ade6d661a02f3e9a2c71951
Budi <aku@budi.my.id>
2019-03-10 11:10:23 +0700

    #1891

--- resources/views/blog/frontend/landing.blade.php
+++ resources/views/blog/frontend/landing.blade.php
@@ -58,94 +58,9 @@ section h3.section-subheading {
   font-family: 'Droid Serif', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
 }
 
-@media (min-width: 768px) {
-  section {
-    padding: 150px 0;
-  }
-}
-
 .service-heading {
   margin: 15px 0;
   text-transform: none;
-}
-
-
-
-#portfolio .portfolio-item {
-  right: 0;
-  margin: 0 0 15px;
-}
-
-#portfolio .portfolio-item .portfolio-link {
-  position: relative;
-  display: block;
-  max-width: 400px;
-  margin: 0 auto;
-  cursor: pointer;
-}
-
-#portfolio .portfolio-item .portfolio-link .portfolio-hover {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  -webkit-transition: all ease 0.5s;
-  transition: all ease 0.5s;
-  opacity: 0;
-  background: rgba(254, 209, 54, 0.9);
-}
-
-#portfolio .portfolio-item .portfolio-link .portfolio-hover:hover {
-  opacity: 1;
-}
-
-#portfolio .portfolio-item .portfolio-link .portfolio-hover .portfolio-hover-content {
-  font-size: 20px;
-  position: absolute;
-  top: 50%;
-  width: 100%;
-  height: 20px;
-  margin-top: -12px;
-  text-align: center;
-  color: white;
-}
-
-#portfolio .portfolio-item .portfolio-link .portfolio-hover .portfolio-hover-content i {
-  margin-top: -12px;
-}
-
-#portfolio .portfolio-item .portfolio-link .portfolio-hover .portfolio-hover-content h3,
-#portfolio .portfolio-item .portfolio-link .portfolio-hover .portfolio-hover-content h4 {
-  margin: 0;
-}
-
-#portfolio .portfolio-item .portfolio-caption {
-  max-width: 400px;
-  margin: 0 auto;
-  padding: 25px;
-  text-align: center;
-  background-color: #fff;
-}
-
-#portfolio .portfolio-item .portfolio-caption h4 {
-  margin: 0;
-  text-transform: none;
-}
-
-#portfolio .portfolio-item .portfolio-caption p {
-  font-size: 16px;
-  font-style: italic;
-  margin: 0;
-  font-family: 'Droid Serif', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-}
-
-#portfolio * {
-  z-index: 2;
-}
-
-@media (min-width: 767px) {
-  #portfolio .portfolio-item {
-    margin: 0 0 30px;
-  }
 }
 
 
@@ -168,11 +83,48 @@ footer ul.quicklinks {
   text-transform: none;
   font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
 }
+
+#service .service-item {
+  right: 0;
+  margin: 0 0 15px;
+}
+
+
+#service .service-item .service-caption {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 25px;
+  text-align: center;
+  background-color: #fff;
+}
+
+#service .service-item .service-caption h4 {
+  margin: 0;
+  text-transform: none;
+  margin-bottom:10px;
+}
+
+#service .service-item .service-caption p {
+  font-size: 16px;
+  margin: 0;
+  
+}
+
+#service * {
+  z-index: 2;
+}
+
+@media (min-width: 767px) {
+  #service .service-item {
+    margin: 0 0 30px;
+  }
+}
 </style>
 @endpush
     
-   <!-- ################################################################### -->
-   <!-- Navigation -->
+<!-- ################################################################### -->
+<!-- Navigation -->
+<!-- ################################################################### -->
   <nav class="navbar navbar-default navbar-expand-lg navbar-dark fixed-top" id="mainNav">
     <div class="container">
       <a class="navbar-brand js-scroll-trigger" href="#page-top">{{ $setting->title1 }}</a>
@@ -206,166 +158,150 @@ footer ul.quicklinks {
       </div>
     </div>
   </nav>
-   
+<!-- ################################################################### -->
+<!-- Header -->   
 <!-- ################################################################### -->
 	<header id="page-top" class="intro-header" style="background-image: url('{{ $setting->header }}'); background-color:#000000">
     	<div class="site-heading">
         	<div class="transbox">
-				<h1 style="padding-top:50px;">{{ $setting->title }}</h1>
+				<h1 style="padding-top:50px;">Find your holiday today with Vertikal Trip</h1>
 				<hr style="max-width:50px;border-color: #c03b44;border-width: 3px;">
 				<p class="text-faded mb-5" style="margin-left:10px; margin-right:10px; margin-bottom:20px; padding:5px; ">
-					{!! nl2br($setting->description) !!}
+					<button type="button" class="btn btn-sm btn-danger">GET STARTED</button>
 				</p>
 			</div>
-             <i class="fa fa-angle-down infinite animated fadeInDown" style="font-size: 50px; color:#FFFFFF; margin-top:30px"></i>
+            <i class="fa fa-angle-down infinite animated fadeInDown" style="font-size: 50px; color:#FFFFFF; margin-top:30px"></i>
        </div>
     </header>
 <!-- ################################################################### -->
-
-
-<section id="services" style="background-image:url('devel/section-2-bg.jpg'); background-position:center; background-repeat:no-repeat; background-size:cover; padding-top:35px; padding-bottom:35px; ">
+<!-- Section -->
+<!-- ################################################################### -->
+<section>
       <div class="container">
+        <!-- ################################################################### -->
         <div class="row" style="padding-bottom:0px;">
           <div class="col-lg-12 text-center">
-            <h3 class="section-heading text-uppercase">Services</h3>
+            <h3 class="section-heading text-uppercase">QUALITIES</h3>
             <h4 class="section-subheading text-muted">Why choose us?</h4>
-            <hr style="max-width:50px;border-color: #e2433b;border-width: 3px;">
+            <hr style="max-width:50px;border-color:#e2433b;border-width:3px;">
           </div>
         </div>
-        <div class="row text-center">
-                    <div class="col-md-4">
-            <span class="fa-stack fa-5x">
-                            <img class="img-fluid" style="border-radius: 50%;" src="devel/trek.png" alt="">
-               
-            </span>
-            <h4 class="service-heading">Trek for a Cause</h4>
-            <p class="text-muted">Not leaving our moral conscience behind, Vertikal Trip is involved in hosting treks keeping in mind the benefit of the society. Setting up medical camps, supporting women empowerment, opting eco- friendly trekking options are some of it.</p>
-          </div>
-                    <div class="col-md-4">
-            <span class="fa-stack fa-5x">
-                            <img class="img-fluid" style="border-radius: 50%;" src="devel/custom.png" alt="">
-               
-            </span>
-            <h4 class="service-heading">Customized Your Adventure</h4>
-            <p class="text-muted">Vertikal Trip brings to you customized trekking, a venture out in the wilderness just how you want it to be. With a group, without one; fun-filled activities; special teenage oriented treks; a romantic isolated candle-lit dinner in the mountains for that perfect proposal; ask for anything and it will be served. As they say, &quot;Your Wish, will be Our Command.&quot;</p>
-          </div>
-                    <div class="col-md-4">
-            <span class="fa-stack fa-5x">
-                            <img class="img-fluid" style="border-radius: 50%;" src="devel/experienced.png" alt="">
-               
-            </span>
-            <h4 class="service-heading">Experienced Trip Leaders</h4>
-            <p class="text-muted">Vertikal Trip enlists among few travel and tour operators that have been founded by the team of experts. Our trek leaders have personally traveled to every popular trekking destinations of Java and thus are well familiar with the things that should be considered while trekking to each specific region. Our experienced wilderness first responders are well trained in first aid and have got leading potentiality. They can devise the best alternative itinerary package for you in case the trekking does not go on schedule due to bad weather and other unforeseen circumstances. Our staffs are professionals who will get focused on the sole purpose of making your stay in the destination as pleasant as it can get.</p>
-          </div>
-                </div>
-    </section>
-     
-
-
-
-<!-- Portfolio Grid -->
-        <section class="bg-light" id="portfolio" style=" padding-top:35px; padding-bottom:35px;">
+        <!-- ################################################################### -->
+        <div class="row text-center justify-content-center">
+        	<!-- ################################################################### -->
+        	<div class="col-lg-3 col-sm-6">
+            	<span class="fa-stack fa-5x">
+            		<img class="img-fluid" style="border-radius: 50%;" src="devel/trek.png" alt="">
+            	</span>
+            	<h4 class="service-heading">Responsible hosting</h4>
+            	<p class="text-muted">You are the main focus of our company, all of our activities are focusing on how we can bring ultimate satisfaction for you</p>
+            </div>
+            <!-- ################################################################### -->
+            <div class="col-lg-3 col-sm-6">
+            	<span class="fa-stack fa-5x">
+            		<img class="img-fluid" style="border-radius: 50%;" src="devel/trek.png" alt="">
+            	</span>
+            	<h4 class="service-heading">Social Impact</h4>
+            	<p class="text-muted">We care about our local community. We guarantee your journey with us will give benefits to them in one or any other way</p>
+            </div>
+            <!-- ################################################################### -->
+            <div class="col-lg-3 col-sm-6">
+            	<span class="fa-stack fa-5x">
+            		<img class="img-fluid" style="border-radius: 50%;" src="devel/trek.png" alt="">
+            	</span>
+            	<h4 class="service-heading">Extraordinary</h4>
+            	<p class="text-muted">We have customized each activity with considering all aspects so you will get the maximum first-class experience in each trip</p>
+            </div>
+            <!-- ################################################################### -->
+            <div class="col-lg-3 col-sm-6">
+            	<span class="fa-stack fa-5x">
+            		<img class="img-fluid" style="border-radius: 50%;" src="devel/trek.png" alt="">
+            	</span>
+            	<h4 class="service-heading">Local &amp; Professional</h4>
+            	<p class="text-muted">We are a group of locals who bring our own expertise to the table to provide the best travel and event experience for you</p>
+            </div>
+        	<!-- ################################################################### -->  
+        </div>
+        <!-- ################################################################### -->
+    </div>
+</section>
+<!-- ################################################################### -->
+<!-- Section -->
+<!-- ################################################################### -->
+<section id="service" style="background-color:#EEEDED">
       <div class="container">
-        <div class="row" style="padding-bottom:0px;">
+        <!-- ################################################################### -->
+        <div class="row" style="padding-bottom:0px; margin-bottom:25px;">
           <div class="col-lg-12 text-center">
-            <h3 class="section-heading text-uppercase">Destinations</h3>
-            <h4 class="section-subheading text-muted">Need help finding the right trek for you ?</h4>
-            <hr style="max-width:50px;border-color: #e2433b;border-width: 3px;">
+            <h3 class="section-heading text-uppercase">Our Services</h3>
+            <h4 class="section-subheading text-muted">Tour packages holiday in Yogyakarta</h4>
+            <hr style="max-width:50px;border-color:#e2433b;border-width:3px;">
           </div>
         </div>
-        
-        <div class="row">
-                 <div class="col-md-4 col-sm-6 portfolio-item">
-          	                <img class="img-fluid" src="devel/merapi.jpg" alt="">
-            <div class="portfolio-caption">
-              <h4>Merapi</h4>
-              <p class="text-muted">2.968 mdpl<br />
-Via Selo</p>
+        <!-- ################################################################### -->
+        <div class="row justify-content-center">
+        	<!-- ################################################################### -->
+        	<div class="col-lg-4 col-sm-6  service-item">
+            		<img class="img-fluid" src="devel/merbabu.jpg" alt="">
+            		<div class="service-caption text-left">
+                		<h4 class="service-heading">Yogyakarta Night Food Tour</h4>
+            			<p class="text-muted">We will invite you enjoying the nighttime atmosphere of Yogyakarta, and discover a variety of distinctive traditional Yogyakarta dishes. Learn interesting fun facts about this city, interact with locals, travel on a becak (Yogyakarta traditional tuk-tuk), and more.</p>
+                        <button type="button" class="btn btn-danger btn-sm" style="margin-top:20px;">Learn more and booking via AirBNB</button>
+                        <button type="button" class="btn btn-warning btn-sm" style="margin-top:20px;">Learn more and booking via Viator</button>
+                	</div>
             </div>
-          </div>
-                  <div class="col-md-4 col-sm-6 portfolio-item">
-          	                <img class="img-fluid" src="devel/merbabu.jpg" alt="">
-            <div class="portfolio-caption">
-              <h4>Merbabu</h4>
-              <p class="text-muted">3.145 mdpl<br />
-Via Selo</p>
+        	<!-- ################################################################### -->
+            <div class="col-lg-4 col-sm-6  service-item">
+            		<img class="img-fluid" src="devel/merbabu.jpg" alt="">
+            		<div class="service-caption text-left">
+                		<h4 class="service-heading">Ullen Sentalu & Merapi Lava Tour</h4>
+            			<p class="text-muted">We will invite you to learn about javanese culture and royal family's life story, explore the javanese hidden treasure, stone architecture and accompany you to enjoy views of the Merapi Mountain's slope by Merapi Lava Tour Jeep</p>
+                        <button type="button" class="btn btn-danger btn-sm" style="margin-top:20px;">Learn more and booking via AirBNB</button>
+                        <button type="button" class="btn btn-warning btn-sm" style="margin-top:20px;">Learn more and booking via Viator</button>
+                	</div>
             </div>
-          </div>
-                  <div class="col-md-4 col-sm-6 portfolio-item">
-          	                <img class="img-fluid" src="devel/sindoro.jpg" alt="">
-            <div class="portfolio-caption">
-              <h4>Sindoro</h4>
-              <p class="text-muted">3.136 mdpl<br />
-Via Kledung</p>
-            </div>
-          </div>
-                  <div class="col-md-4 col-sm-6 portfolio-item">
-          	                <img class="img-fluid" src="/devel/sumbing.jpg" alt="">
-            <div class="portfolio-caption">
-              <h4>Sumbing</h4>
-              <p class="text-muted">3.371 mdpl<br />
-Via Kledung</p>
-            </div>
-          </div>
-                  <div class="col-md-4 col-sm-6 portfolio-item">
-          	                <img class="img-fluid" src="devel/prau.jpg" alt="">
-            <div class="portfolio-caption">
-              <h4>Prau</h4>
-              <p class="text-muted">2.565 mdpl<br />
-Via Pathak Banteng</p>
-            </div>
-          </div>
-                  <div class="col-md-4 col-sm-6 portfolio-item">
-          	                <img class="img-fluid" src="devel/lawu.jpg" alt="">
-            <div class="portfolio-caption">
-              <h4>Lawu</h4>
-              <p class="text-muted">3.265 mdpl<br />
-Via Cemoro Sewu</p>
-            </div>
-          </div>
-          
-          
+        	<!-- ################################################################### -->
         </div>
-      </div>
-    </section>
-    
-    
-    
-    <!-- Footer -->
+        <!-- ################################################################### -->
+    </div>
+</section>
+<!-- ################################################################### --> 
+<!-- Footer -->
+<!-- ################################################################### -->
     <footer>
       <div class="container">
         <div class="row">
           <div class="col-md-4 text-left">
             <span class="copyright"><strong>Phone :</strong> +1 505 585 2112</span><br>
-            <span class="copyright"><strong>Email :</strong> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="2245574b464762544750564b49434e56504b520c414d4f">[email&#160;protected]</a></span><br>
-            <span class="copyright"><strong>Address :</strong>SUTODIRJAN GT2/837 YOGYAKARTA 55272</span><br>
+            <span class="copyright"><strong>Email :</strong> guide@vertikaltrip.com</span><br>
+            <span class="copyright"><strong>Address :</strong> SUTODIRJAN GT2/837 YOGYAKARTA 55272</span><br>
           </div>
           <div class="col-md-4">
-            <ul class="list-inline social-buttons">
+            <ul class="list-inline">
               <li class="list-inline-item">
-                <img src="devel/mastercard.jpg">
+                <img width="80" src="devel/mastercard.jpg">
               </li>
               <li class="list-inline-item">
-                <img src="devel/paypal.jpg">
+                <img width="80" src="devel/paypal.jpg">
               </li>
               <li class="list-inline-item">
-                <img src="devel/visa.jpg">
+                <img width="80" src="devel/visa.jpg">
               </li>
             </ul>
           </div>
           <div class="col-md-4">
             <ul class="list-inline quicklinks">
               <li class="list-inline-item">
-                Terms and Cancellation Policy
+                <a href="#">Terms and Cancellation Policy</a>
               </li>
               <li class="list-inline-item">
-                Safety and Emergency
+                <a href="#">Safety and Emergency</a>
               </li>
               <li class="list-inline-item">
-                Privacy Policy
+                <a href="#">Privacy Policy</a>
               </li>
               <li class="list-inline-item">
-                Disclaimer
+                <a href="#">Disclaimer</a>
               </li>
             </ul>
           </div>
