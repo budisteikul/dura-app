cdf14f588eff1cc9d87180074a161e000815053e
Budi <aku@budi.my.id>
2019-03-15 23:49:30 +0700

    #1891

--- app/Jobs/RCloneImages.php
+++ app/Jobs/RCloneImages.php
@@ -31,7 +31,7 @@ class RCloneImages implements ShouldQueue
      */
     public function handle()
     {
-        $process = new Process(array('rclone','--config=/home/pi/.rclone.conf', 'sync', '/home/pi/public_html/pi_aplikasi/public/storage/eca1ca75-9e80-493f-bfef-cbeb44f8aac3/images/original/', 'gdrive:/ratnawahyu.com/'));
+        $process = new Process(array('rclone','--config=/home/pi/.rclone.conf', 'sync', '/home/pi/public_html/pi_aplikasi/storage/app/public/eca1ca75-9e80-493f-bfef-cbeb44f8aac3/images/original/', 'gdrive:/ratnawahyu.com/'));
 		$process->run();
     }
 }
