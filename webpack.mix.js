e135391e8a5651f6ac5a736ee33e3fa6ac743537
Budi <aku@budi.my.id>
2019-09-10 04:16:23 -0400

    #1891

--- webpack.mix.js
+++ webpack.mix.js
@@ -85,16 +85,6 @@ mix.webpackConfig({
         minify: true,
         stripPrefix: 'public/',
         handleFetch: true,
-        dynamicUrlToDependencies: { 
-			//you should add the path to your blade files here so they can be cached
-            //and have full support for offline first (example below)
-            '/': ['resources/views/blog/frontend/foodtour.blade.php'],
-            '/book': ['resources/views/blog/frontend/timeselector-stripe.blade.php'],
-			'/book/checkout': ['resources/views/blog/frontend/checkout-stripe.blade.php'],
-			'/book/checkout/receipt': ['resources/views/blog/frontend/receipt-stripe.blade.php'],
-			'/order': ['resources/views/blog/frontend/product.blade.php'],
-			'/tour/yogyakarta-night-walking-and-food-tours': ['resources/views/blog/frontend/product.blade.php'],
-        },
         staticFileGlobsIgnorePatterns: [/\.map$/, /mix-manifest\.json$/, /manifest\.json$/, /service-worker\.js$/],
         navigateFallback: '/',
         runtimeCaching: [
