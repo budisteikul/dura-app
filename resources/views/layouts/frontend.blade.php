ff27985bb6a78f1d0aee55a3080b4930e4301a82
budi <budi@budis-iMac.local>
2019-09-15 09:06:06 +0700

    #1891

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -54,6 +54,8 @@ s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
+
+
 </script>
 <!--End of Tawk.to Script-->
     
@@ -67,6 +69,41 @@ s0.parentNode.insertBefore(s1,s0);
 
 </head>
 <body class=" bg-dark">
+<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+<script>
+$(window).on('load', function(){
+	$(".se-pre-con").fadeOut("slow");
+});
+</script>
+<style>
+.no-js #loader { display: none;  }
+.js #loader { display: block; position: absolute; left: 100px; top: 0; }
+.se-pre-con {
+	position: fixed;
+	left: 0px;
+	top: 0px;
+	width: 100%;
+	height: 100%;
+	z-index: 9999;
+	background: url(/img/LoadingMountains.svg) center no-repeat #fff;
+	background-size: 10% 10%;
+}
+@media(max-width:767px) {
+	.no-js #loader { display: none;  }
+	.js #loader { display: block; position: absolute; left: 100px; top: 0; }
+	.se-pre-con {
+		position: fixed;
+		left: 0px;
+		top: 0px;
+		width: 100%;
+		height: 100%;
+		z-index: 9999;
+		background: url(/img/LoadingMountains.svg) center no-repeat #fff;
+		background-size: 50% 50%;
+	}
+}
+</style>
+<div class="se-pre-con"></div>
 
 	@yield('content')
 
