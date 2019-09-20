8f1d528f10c40ce64ab32c7ffdd0e7b1e8f1b348
Budi <aku@budi.my.id>
2019-09-20 22:10:54 +0700

    #1891

---
+++ resources/views/blog/frontend/blank.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.bokun')
+@section('content')
+@push('scripts')
+
+
+@endpush
+    
+<!-- ################################################################### -->
+<!-- Navigation -->
+
+<section id="booking" style="background-color:#ffffff">
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12 col-md-12 mx-auto">
+			<div class="row" style="padding-bottom:0px;">
+				<div class="col-lg-12 text-center">
+				<div style="height:70px;"></div>	
+                
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Type payments</a></li>
+    <li class="breadcrumb-item"><a href="#">Select options</a></li>
+	<li class="breadcrumb-item"><a href="#">Check out</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Receipt</li>
+  </ol>
+</nav>
+					
+				<div style="height:45px;"></div>		
+				</div>
+			</div>
+        </div>
+	</div>
+</div>
+</section>
+
+
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+@endsection
