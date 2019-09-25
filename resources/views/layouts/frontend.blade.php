423b74fee5d1a8a70fb9a44b871e21d3339c3695
Budi <aku@budi.my.id>
2019-09-25 17:49:02 +0700

    #1891

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -98,7 +98,7 @@ s0.parentNode.insertBefore(s1,s0);
 <i class="fa fa-phone-square"></i> Phone : +1 505-585-2112
                 <br>
                 <br>
-				<b>Vertikal Trip Team</b>
+				<b>Jogja Food Tour Team</b>
 				<br>
 				Perum. Guwosari Blok XII
 				<br>
