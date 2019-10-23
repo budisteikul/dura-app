26937e3fb23c751b3e23d549a7698c01547ad547
Budi <aku@budi.my.id>
2019-10-23 16:03:10 +0700

    #1891

--- resources/views/blog/frontend/blank.blade.php
+++ resources/views/blog/frontend/blank.blade.php
@@ -1,232 +1,81 @@
+@extends('layouts.frontend')
+@section('content')
+@push('scripts')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="google-site-verification" content="AtDcFU1Yadtr8qSpQQL86nxPnXGXAgveC60d4oZq0U0" />
-    <meta name="description" content="Enjoy Jogja in Local Ways. Join us on this experience to try authentic Javanese dishes, play traditional games, travel on a becak, learn interesting fun facts about city, interact with locals and many more.">
-    <meta name="author" content="Vertikal Trip">
-    <meta name="robots" content="all,index,follow">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-	<link rel="manifest" href="/manifest.json">
-	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#2e3237">
-	<meta name="msapplication-TileColor" content="#2e3237">
-	<meta name="theme-color" content="#2e3237">
+
+@endpush
     
-    <title>Yogyakarta Night Walking and Food Tours</title>
-    
-    
-	<link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css' media="screen,handheld">
-    
-    
-    
-	<script src="https://static.budi.my.id/js/vertikaltrip-1.0.8.js"></script>
-	<link href="https://static.budi.my.id/css/vertikaltrip-1.0.8.css" rel="stylesheet" media="screen,handheld">
-    
-    
-  
-    
-<!-- script>
-          if ('serviceWorker' in navigator ) {
-            window.addEventListener('load', function() {
-				caches.delete('sw-precache-v3-pwa-');
-                navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-                    // Registration was successful
-                    console.log('ServiceWorker registration successful with scope: ', registration.scope);
-                }, function(err) {
-                    // registration failed :(
-                    console.log('ServiceWorker registration failed: ', err);
-                });
-            });
-        }
-</script -->    
-    
-    @stack('scripts')
-	
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5d1810cb22d70e36c2a3697f/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-
-
-</script>
-<!--End of Tawk.to Script-->
-    
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-     
-
-</head>
-<body class=" bg-dark">
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<!-- ################################################################### -->
 <!-- Navigation -->
-<nav class="navbar navbar-default navbar-expand-lg navbar-light  bg-light fixed-top shadow mb-5" id="mainNav">
+<nav class="navbar navbar-default navbar-expand-lg navbar-dark fixed-top shadow mb-5" id="mainNav-back">
 	<div class="container">
-    
+		<script type="text/javascript" src="https://widgets.bokun.io/assets/javascripts/apps/build/BokunWidgetsLoader.js?bookingChannelUUID=af09519a-6eee-4a10-b127-5bd5edb0f3ce" async></script>
+      <style> #bokun_1f969eb1_3d73_49e5_8a98_6481cf12b1c5 { display: inline-block; padding: 5px 10px; background: #21d41b; border-radius: 5px; box-shadow: none; font-weight: 600; font-size: 14px; text-decoration: none; text-align: center; color: #FFFFFF; font-family: sans-serif; cursor: pointer; } #bokun_1f969eb1_3d73_49e5_8a98_6481cf12b1c5:hover{ background: #169012; } #bokun_1f969eb1_3d73_49e5_8a98_6481cf12b1c5:active{ background: #1aa715; } </style> <button class="bokunButton" disabled id=bokun_1f969eb1_3d73_49e5_8a98_6481cf12b1c5 data-src="https://widgets.bokun.io/online-sales/af09519a-6eee-4a10-b127-5bd5edb0f3ce/experience-calendar/208273?partialView=1" > Book now </button> 
+	</div>
+</nav>   
+<div style="height:25px;"></div>
 
-<script type="text/javascript" src="https://widgets.bokun.io/assets/javascripts/apps/build/BokunWidgetsLoader.js?bookingChannelUUID=63652e4f-478e-47b6-8e0e-6e019f4072e0" async></script>
-      <style> #bokun_d81c3320_52b8_4216_868e_f35035a50868 { display: inline-block; padding: 10px 20px; background: #408C3D; border-radius: 5px; box-shadow: none; font-weight: 600; font-size: 16px; text-decoration: none; text-align: center; color: #FFFFFF; font-family: sans-serif; cursor: pointer; } #bokun_d81c3320_52b8_4216_868e_f35035a50868:hover{ background: #285726; } #bokun_d81c3320_52b8_4216_868e_f35035a50868:active{ background: #30682e; } </style> <button class="bokunButton" disabled id=bokun_d81c3320_52b8_4216_868e_f35035a50868 data-src="https://widgets.bokun.io/online-sales/63652e4f-478e-47b6-8e0e-6e019f4072e0/experience-calendar/234927?partialView=1" > Book now </button> 
 
+<section id="booking" style="background-color:#ffffff">
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12 col-md-12 mx-auto">
+			<div class="row" style="padding-bottom:0px;">
+				<div class="col-lg-12 text-center">
+				<div style="height:70px;"></div>	
+        
 
-
-		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse stroke" id="navbarResponsive">
-			<ul class="navbar-nav text-uppercase ml-auto">
-				<li class="nav-item">
-					<a class="nav-link js-scroll-trigger" href="#services">Services</a>
-				</li>
-                
-                <li class="nav-item">
-					<a class="nav-link js-scroll-trigger" href="#about">The Tour</a>
-				</li>
-                
-				<li class="nav-item">
-					<a class="nav-link js-scroll-trigger" href="#gallery">Snapshot</a>
-				</li>
-                
-                <li class="nav-item">
-					<a class="nav-link js-scroll-trigger" href="#review">Reviews</a>
-				</li>
-                
-                <li class="nav-item">
-					<a class="nav-link js-scroll-trigger" href="#guide">Tour Guide</a>
-				</li>
-			</ul>
-		</div>
+                    
+					<div class="row" style="padding-bottom:0px;">
+						<div class="col-lg-12 text-center">
+							<!-- h3 class="section-heading" style="margin-top:0px;">Payment method</h3 -->
+							<h4 class="section-subheading text-muted">Which payment would you like to use?</h4>
+							<hr style="max-width:50px;border-color:#e2433b;border-width:3px;">
+						</div>
+					</div>
+					
+                    <div class="row col-md-8  mx-auto">
+    <div class="col-sm-6 first-column">
+      <div class="bd-callout bd-callout-danger text-left text-secondary shadow p-3 bg-white rounded" style="margin-right:5px; height:180px">
+                    	<div style="margin-bottom:8px;">
+						<i class="fab fa-cc-mastercard fa-2x"></i> <i class="fab fa-cc-visa fa-2x"></i> <i class="fab fa-cc-discover fa-2x"></i> <i class="fab fa-cc-amex fa-2x"></i> <i class="fab fa-cc-diners-club fa-2x"></i>
+						</div>
+                        <div style="margin-bottom:8px; height:50px;">
+						<!-- i class="fab fa-cc-mastercard fa-2x"></i> <i class="fab fa-cc-visa fa-2x"></i> <i class="fab fa-cc-discover fa-2x"></i> <i class="fab fa-cc-amex fa-2x"></i> <i class="fab fa-cc-diners-club fa-2x"></i -->
+                        <img src="/assets/foodtour/3dsecure.jpg" height="50">
+                        </div>
+                        <div style="margin-bottom:8px;">
+                        <a class="btn btn-danger" href="/book/stripe"><i class="fa fa-credit-card"></i> <span style="font-family: 'Barlow','Helvetica Neue',Arial,sans-serif;"><strong>Use Credit Card Payment</strong></span></a>
+                        </div>
+					</div>
     </div>
-  </nav>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<section id="booking" style="background-color:#ffffff;">
-<div id="bokun-w85603_0f7a2cef_4f99_4090_a118_1d06b01b9f90">Loading...</div><script type="text/javascript">
-var w85603_0f7a2cef_4f99_4090_a118_1d06b01b9f90;
-(function(d, t) {
-  var host = 'widgets.bokun.io';
-  var frameUrl = 'https://' + host + '/widgets/85603?bookingChannelUUID=63652e4f-478e-47b6-8e0e-6e019f4072e0&amp;activityId=234927&amp;lang=en&amp;ccy=USD&amp;hash=w85603_0f7a2cef_4f99_4090_a118_1d06b01b9f90';
-  var s = d.createElement(t), options = {'host': host, 'frameUrl': frameUrl, 'widgetHash':'w85603_0f7a2cef_4f99_4090_a118_1d06b01b9f90', 'autoResize':true,'height':'','width':'100%', 'minHeight': 0,'async':true, 'ssl':true, 'affiliateTrackingCode': '', 'transientSession': true, 'cookieLifetime': 43200 };
-  s.src = 'https://' + host + '/assets/javascripts/widgets/embedder.js';
-  s.onload = s.onreadystatechange = function() {
-    var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
-    try {
-      w85603_0f7a2cef_4f99_4090_a118_1d06b01b9f90 = new BokunWidgetEmbedder(); w85603_0f7a2cef_4f99_4090_a118_1d06b01b9f90.initialize(options); w85603_0f7a2cef_4f99_4090_a118_1d06b01b9f90.display();
-    } catch (e) {}
-  };
-  var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
-})(document, 'script');
-</script>
+    <div class="col-sm-6 second-column">
+      <div class="bd-callout bd-callout-danger text-left text-secondary shadow p-3 bg-white rounded" style="margin-right:5px; height:180px">
+                    	<div style="margin-bottom:8px;">
+						<i class="fab fa-cc-paypal fa-2x"></i>
+						</div>
+                        <div style="margin-bottom:8px; height:50px;">
+						<!-- i class="fab fa-cc-paypal fa-2x"></i --><img src="/assets/foodtour/logo-paypal.png" height="40">
+                        </div>
+                        <div style="margin-bottom:8px;">
+                        <a class="btn btn-danger" href="/book/paypal"><i class="fab fa-paypal"></i> <span style="font-family: 'Barlow','Helvetica Neue',Arial,sans-serif;"><strong>Use PayPal Payment</strong></span></a>
+                        </div>
+                    </div>
+    </div> 
+  </div>
+                    
+					
+					
+					
+					
+				<div style="height:45px;"></div>		
+				</div>
+			</div>
+        </div>
+	</div>
+</div>
 </section>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<footer class="py-5 bg-light">
-<div class="container">
-    <div class="row">
-		<div class="row col-md-8">
-            	<div class="col-sm-6 first-column mb-4">
-                	<p class="m-0 text-left">
-                    	<b class="text-danger">JOGJA FOOD TOUR TEAM</b>
-						<br>
-                		Customer Support and Operation Office
-                		<br>
-						Jalan Abiyoso VII No.190
-                        <br>
-                        Bantul, DIY 55751
-                        <br>
-						Indonesia
-						<br>
-						<span class="fab fa-whatsapp"></span> Whatsapp : <a href="https://wa.me/+6285743112112">+62 857-4311-2112</a> <br>
-						<span class="fa fa-envelope"></span> Email : <a href="mailto:guide@vertikaltrip.com" target="_blank">guide@vertikaltrip.com</a>
-						<br>
-                        
-						
-                    </p>
-                </div>
-                <div class="col-sm-4 second-column">
-                	<p class="m-0 text-left">
-                    	
-                    	<b class="text-danger">VERTIKAL TRIP LLC</b>
-                		<br>
-                		16192 Coastal Highway
-						<br>Lewes, DE 19958
-                        <br>
-                        United States
-						<br>
-                    </p>
-                </div>
-			
-        </div>
-    </div>
-</div>
-</footer>
-</div>
-
-</body>
-</html>
+@endsection
