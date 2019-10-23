d51d733a92f95767a85cf57e39a68b1eae1b26d8
Budi <aku@budi.my.id>
2019-10-23 16:10:47 +0700

    #1891

--- resources/views/blog/frontend/foodtour.blade.php
+++ resources/views/blog/frontend/foodtour.blade.php
@@ -69,7 +69,7 @@
     	<!-- Noscript content for added SEO -->
 <noscript><a href="https://jogjafoodtour.eventbrite.com" rel="noopener noreferrer" target="_blank"></noscript>
 <!-- You can customize this button any way you like -->
-<button id="eventbrite-widget-modal-trigger-77732854059" type="button">Buy Tickets</button>
+<button id="eventbrite-widget-modal-trigger-77732854059" class="btn btn-danger" type="button">Buy Tickets</button>
 <noscript></a>Buy Tickets on Eventbrite</noscript>
 
 <script src="https://www.eventbrite.com/static/widgets/eb_widgets.js"></script>
