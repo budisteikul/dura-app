95fa1510f0a5b7babeae131b2f09a310408327e9
Budi <aku@budi.my.id>
2019-11-01 03:01:03 +0700

    #1891

--- resources/views/blog/frontend/foodtour.blade.php
+++ resources/views/blog/frontend/foodtour.blade.php
@@ -68,7 +68,7 @@
 	<div class="container">
 
 
-<noscript><a href="https://jogjafoodtour.eventbrite.com" rel="noopener noreferrer" target="_blank"></noscript>
+<!-- noscript><a href="https://jogjafoodtour.eventbrite.com" rel="noopener noreferrer" target="_blank"></noscript>
 
 <button class="btn btn-danger text-white" id="eventbrite-widget-modal-trigger-77732854059" type="button"><i class="fa fa-ticket-alt"></i> <span style="font-family: 'Barlow','Helvetica Neue',Arial,sans-serif;"><strong>Book now</strong></span></button>
 <noscript></a>Book now on Eventbrite</noscript>
@@ -85,10 +85,10 @@
         modalTriggerElementId: 'eventbrite-widget-modal-trigger-77732854059',
         onOrderComplete: exampleCallback
     });
-</script>
+</script -->
 
 
-<!-- a class="btn btn-danger text-white" href="/tour/yogyakarta-night-walking-and-food-tours/time_selector"><i class="fa fa-ticket-alt"></i> <span style="font-family: 'Barlow','Helvetica Neue',Arial,sans-serif;"><strong>Book now</strong></span></a -->
+<a class="btn btn-danger text-white" href="/tour/yogyakarta-night-walking-and-food-tours/time_selector"><i class="fa fa-ticket-alt"></i> <span style="font-family: 'Barlow','Helvetica Neue',Arial,sans-serif;"><strong>Book now</strong></span></a>
 
        
 		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
