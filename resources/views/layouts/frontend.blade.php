66bba929a2a8e17f170d52ff6fa1866baae5db15
Budi <aku@budi.my.id>
2019-10-31 15:22:54 +0700

    #1891

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -24,8 +24,8 @@
     
     
     
-	<script src="https://static.budi.my.id/js/vertikaltrip-1.0.9.js"></script>
-	<link href="https://static.budi.my.id/css/vertikaltrip-1.0.9.css" rel="stylesheet" media="screen,handheld">
+	<script src="/js/vertikaltrip-1.0.9.js"></script>
+	<link href="/css/vertikaltrip-1.0.9.css" rel="stylesheet" media="screen,handheld">
     
     
   
