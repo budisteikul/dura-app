0ba724b33a2c7893276acbe04e7ad290753f5317
Budi <aku@budi.my.id>
2019-11-17 12:16:24 +0700

    #1891

--- resources/views/blog/frontend/foodtour.blade.php
+++ resources/views/blog/frontend/foodtour.blade.php
@@ -85,6 +85,24 @@
         onOrderComplete: exampleCallback
     });
 </script>
+@elseif(str_ireplace("www.","",$_SERVER['HTTP_HOST'])=="jogjafoodtour.com")
+<noscript><a href="https://jogjafoodtour.eventbrite.com" rel="noopener noreferrer" target="_blank"></noscript>
+<button class="btn btn-danger text-white" id="eventbrite-widget-modal-trigger-77732854059" type="button"><i class="fa fa-ticket-alt"></i> <span style="font-family: 'Barlow','Helvetica Neue',Arial,sans-serif;"><strong>Book now</strong></span></button>
+<noscript></a>Book now on Eventbrite</noscript>
+<script src="https://www.eventbrite.com/static/widgets/eb_widgets.js"></script>
+<script type="text/javascript">
+    var exampleCallback = function() {
+        console.log('Order complete!');
+    };
+
+    window.EBWidgets.createWidget({
+        widgetType: 'checkout',
+        eventId: '77732854059',
+        modal: true,
+        modalTriggerElementId: 'eventbrite-widget-modal-trigger-77732854059',
+        onOrderComplete: exampleCallback
+    });
+</script>
 @else
 <a class="btn btn-danger text-white" href="/tour/yogyakarta-night-walking-and-food-tours/time_selector"><i class="fa fa-ticket-alt"></i> <span style="font-family: 'Barlow','Helvetica Neue',Arial,sans-serif;"><strong>Book now</strong></span></a>
 @endif
