224fb86bf8b1d847a37a251a866213346e3e9867
Budi <aku@budi.my.id>
2019-12-29 21:26:03 +0700

    #1891

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -82,81 +82,36 @@ s0.parentNode.insertBefore(s1,s0);
 
 <footer class="py-5 bg-dark">
 <div class="container">
-<div class="row">
-	<div class="row mx-auto">
-    				<p class="m-0 text-center text-white">
-					@if(str_ireplace("www.","",$_SERVER['HTTP_HOST'])=="jogjafoodtour.com")
-						<img src="/logo.png" alt="VERTIKAL TRIP LLC" height="50">
-						<br>
-						
-                		
-						Jalan Abiyoso VII No.190
-                        <br>
-                        Bantul, DIY 55751 Indonesia
-						<br>
-						<b>Info and Reservation</b>
-						<br>
-						<span class="fab fa-whatsapp"></span> Whatsapp : <a class="badge badge-danger" href="https://wa.me/+6285743112112">+62 857-4311-2112</a> <br>
-						<span class="fa fa-envelope"></span> Email : <a href="mailto:guide@jogjafoodtour.com" class="badge badge-danger" target="_blank">guide@vertikaltrip.com</a>
-						
-						<br>
-						<img src="/assets/foodtour/PP_AcceptanceMarkTray-NoDiscover_243x40.png" height="35" alt="Buy now with PayPal"  style="margin-top:10px;margin-bottom:5px;">
-					
-						<!-- i class="fab fa-cc-paypal fa-2x"></i>
-						<i class="fab fa-cc-mastercard fa-2x"></i>
-						<i class="fab fa-cc-visa fa-2x"></i>
-						<i class="fab fa-cc-amex fa-2x"></i>
-						<i class="fab fa-cc-discover fa-2x"></i -->
-					@else
-						<img src="/logo.png" alt="VERTIKAL TRIP LLC" height="50">
-						<br>
-						
-                		
-						Jalan Abiyoso VII No.190
-                        <br>
-                        Bantul, DIY 55751 Indonesia
-						<br>
-						<b>Info and Reservation</b>
-						<br>
-						<span class="fab fa-whatsapp"></span> Whatsapp : <a class="badge badge-danger" href="https://wa.me/+6285743112112">+62 857-4311-2112</a> <br>
-						<span class="fa fa-envelope"></span> Email : <a href="mailto:guide@jogjafoodtour.com" class="badge badge-danger" target="_blank">guide@vertikaltrip.com</a>
-						
-						<br>
-						
-						<img src="/assets/foodtour/PP_AcceptanceMarkTray-NoDiscover_243x40.png" height="35" alt="Buy now with PayPal"  style="margin-top:10px;margin-bottom:5px;">
-					@endif
-                    	
-                        
-                        <br>
-						
-                    </p>
-    </div>
-</div>
-    <!-- div class="row">
+    <div class="row">
 		<div class="row col-md-8">
            	
             	<div class="col-sm-6 first-column mb-4">
                 	<p class="m-0 text-left text-white">
-                    	<b class="text-danger">VERTIKAL TRIP LLC</b>
+                    	<img src="/logo.png" alt="VERTIKAL TRIP LLC" height="50">
 						<br>
-                		Jogja Food Tour Team
-                		<br>
 						Jalan Abiyoso VII No.190
                         <br>
                         Bantul, DIY 55751 Indonesia
 						<br>
-						<span class="fab fa-whatsapp"></span> Whatsapp : <a class="badge badge-danger" href="https://wa.me/+6285743112112">+62 857-4311-2112</a> <br>
-						<span class="fa fa-envelope"></span> Email : <a href="mailto:guide@jogjafoodtour.com" class="badge badge-danger" target="_blank">guide@jogjafoodtour.com</a>
 						<br>
-                        <img src="/assets/foodtour/PP_AcceptanceMarkTray-NoDiscover_243x40.png" height="35" alt="Buy now with PayPal"  style="margin-top:10px;margin-bottom:5px;"  />
-                        <br>
+						<b>INFO AND RESERVATION</b>
+						<br>
+						<span class="fab fa-whatsapp"></span> Whatsapp : <a class="badge badge-danger" href="https://wa.me/+6285743112112">+62 857-4311-2112</a> <br>
+						<span class="fa fa-envelope"></span> Email : <a href="mailto:guide@jogjafoodtour.com" class="badge badge-danger" target="_blank">guide@vertikaltrip.com</a>
 						
                     </p>
                 </div>
                 
                 <div class="col-sm-5 second-column">
                 	<p class="m-0 text-left text-white">
-                    	
+                    	<b>PAYMENT CHANNEL</b>
+						
+						<br>
+						<img src="/assets/foodtour/bdg_secured_by_pp_2line.png" style="margin-top:5px;" alt="Secured by Paypal" height="30">
+						
+						<br>
+						<img src="/assets/foodtour/PP_AcceptanceMarkTray-NoDiscover_243x40.png" height="35" alt="Buy now with PayPal"  style="margin-top:10px;margin-bottom:5px;">
+					
                     </p>
                 </div>
 			
@@ -164,7 +119,7 @@ s0.parentNode.insertBefore(s1,s0);
         
        
         
-    </div -->
+    </div>
 </div>
 </footer>
 
