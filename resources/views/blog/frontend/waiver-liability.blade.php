5afab6d0d794fac3dece4dd4c3f07b32f38c3abb
Budi <aku@budi.my.id>
2020-01-10 08:56:22 +0700

    #1891

--- resources/views/blog/frontend/waiver-liability.blade.php
+++ resources/views/blog/frontend/waiver-liability.blade.php
@@ -4,142 +4,131 @@
 
 
 @endpush
-    
-<!-- ################################################################### -->
-<!-- Navigation -->
-<nav class="navbar navbar-default navbar-expand-lg navbar-dark fixed-top shadow mb-5" id="mainNav-back">
-	<div class="container">
-		<noscript><a href="https://jogjafoodtour.eventbrite.com" rel="noopener noreferrer" target="_blank"></noscript>
-
-<button style="margin-top:6px; margin-bottom:6px;" class="btn btn-danger text-white" id="eventbrite-widget-modal-trigger-77732854059" type="button"><i class="fa fa-ticket-alt"></i> <span style="font-family: 'Barlow','Helvetica Neue',Arial,sans-serif;"><strong>Book now</strong></span></button>
-<noscript></a>Book now on Eventbrite</noscript>
-<script src="https://www.eventbrite.com/static/widgets/eb_widgets.js"></script>
-<script type="text/javascript">
-    var exampleCallback = function() {
-        console.log('Order complete!');
-    };
-
-    window.EBWidgets.createWidget({
-        widgetType: 'checkout',
-        eventId: '77732854059',
-        modal: true,
-        modalTriggerElementId: 'eventbrite-widget-modal-trigger-77732854059',
-        onOrderComplete: exampleCallback
-    });
-</script>
-
-
-<!-- a class="btn btn-danger text-white" href="/tour/yogyakarta-night-walking-and-food-tours/time_selector"><i class="fa fa-ticket-alt"></i> <span style="font-family: 'Barlow','Helvetica Neue',Arial,sans-serif;"><strong>Book now</strong></span></a -->
-        
-        
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse stroke" id="navbarResponsive">
-			<ul class="navbar-nav text-uppercase ml-auto">
-            	<li class="nav-item">
-					<a class="nav-link" href="/">Home</a>
-				</li>
-                
-				<li class="nav-item">
-					<a class="nav-link" href="/#services">Why Jogja Food Tour?</a>
-				</li>
-                
-                <li class="nav-item">
-					<a class="nav-link" href="/#about">The Tour</a>
-				</li>
-                
-				<li class="nav-item">
-					<a class="nav-link" href="/#gallery">Snapshot</a>
-				</li>
-                
-                <li class="nav-item">
-					<a class="nav-link" href="/#review">Reviews</a>
-				</li>
-                
-                <li class="nav-item">
-					<a class="nav-link" href="/#guide">Tour Guide</a>
-				</li>
-			</ul>
-		</div>
-	</div>
-</nav>   
 <div style="height:25px;"></div>
 
+<style>
+
+@media (max-width: 61.99875em)
+{
+.dOqElu {
+    padding: 0px 16px;
+    margin: 16px;
+}
+}
+
+.dOqElu {
+    position: relative;
+    width: 100%;
+    max-width: 1280px;
+    margin: 32px 16px;
+}
+
+.jUjHxE {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px -8px -16px;
+}
+.jUjHxE .productList {
+    padding-right: 8px;
+    padding-left: 8px;
+    margin-bottom: 16px;
+}
+
+@media (max-width: 47.99875em)
+{
+.iXGSwc {
+    max-width: 100%;
+    flex: 0 0 100%;
+    margin: 8px auto 16px;
+}
+}
+
+@media (max-width: 61.99875em)
+{
+.iXGSwc {
+    max-width: 100%;
+    flex: 0 0 50%;
+    margin: 24px auto 16px;
+}
+}
+
+.iXGSwc {
+    display: flex;
+    max-width: 33.3%;
+    padding-right: 24px;
+    padding-left: 24px;
+    margin-bottom: 32px;
+    flex: 0 0 33.3%;
+}
+
+.jDZbae {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    box-shadow: rgba(0, 0, 0, 0.2) 0px 4px 6px 0px;
+    overflow-wrap: break-word;
+    overflow: hidden;
+    background: rgb(255, 255, 255);
+    border-radius: 5px;
+    transition: all 0.4s ease 0s;
+}
+
+.jQRdbD {
+    position: relative;
+    line-height: 0;
+    isolation: isolate;
+    overflow: hidden;
+    border-radius: 5px;
+    transition: all 0.4s ease 0s;
+}
+
+.jCDvDq {
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    text-align: center;
+    flex: 1 1 0%;
+    padding: 15px;
+}
+
+.cSIgnb {
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+}
+</style>
 
 <section id="booking" style="background-color:#ffffff">
-<div class="container">
-	<div class="row">
-		<div class="col-lg-12 col-md-12 mx-auto">
-			<div class="row" style="padding-bottom:0px;">
-				<div class="col-lg-12 text-center">
-				<div style="height:70px;"></div>	
-        
 
-                    
-					<div class="row" style="padding-bottom:0px;">
-						<div class="col-lg-12 text-center">
-							<!-- h3 class="section-heading" style="margin-top:0px;">Payment method</h3 -->
-							<h4 class="section-subheading text-muted">WAIVER AND RELEASE OF ALL CLAIMS</h4>
-							<hr style="max-width:50px;border-color:#e2433b;border-width:3px;">
-						</div>
+		
+<div class="do1fk2-0 sc-19acsa7-0 dOqElu">
+	<div class="sc-19acsa7-1 jUjHxE">
+		<div class="sc-2uw1sw-0 iXGSwc productList">
+			<div class="sc-2uw1sw-1 jDZbae">
+				<div class="sc-2uw1sw-2 jQRdbD">
+					<div class="sc-2uw1sw-3 eVtyOz"></div>
+						<div class="sc-2uw1sw-4 hRjJfL" style="background-image: url('https://bokunprod.imgix.net/00896c0f-7c1b-4246-9e41-7d2b52c63a69.jpeg'); background-size: cover;"></div>
 					</div>
-					
-                    <div class="row col-md-8  mx-auto text-left">
-    
-                      <p>By virtue of purchasing tickets for and/or participating in the food tasting tours offered by VERTIKAL TRIP LLC (the “Company”), and in consideration of being allowed to purchase tickets, and for other good and valuable consideration, the receipt and sufficiency of which is acknowledged, purchaser/tour guest (the “Guest”) understands, acknowledges represents, warrants and agrees as follows, with the knowledge that the Company will rely on same: <br><br>
-                        A. Participant desires to participate in the food tasting and cultural walking tours offered by the Company (the “Tours”). I acknowledge that this Waiver and Release of Claims Form will be used by the Tours, sponsors, and organizers of the activity in which I may participate, and that it will govern my actions and responsibilities at said activity.
-  <br><br>
-                        B. Participant certifies that he or she is in good health, and does not suffer from any physical or mental disability that would prevent participant to take part in the activities offered by the Tours and have not been advised to not participate in said type of activities by a qualified medical professional. Participant consents to receive medical treatment which may be deemed advisable in the event of injury, accident, and/or illness during the tour.
-  <br><br>
-                        C. Participant fully comprehends and accepts all of the risks associated with his/her participation in the Tours including, without limitation, exposure to unfavorable weather conditions, food sickness, injuries (e.g., without limitation, those arising out of self-inflicted accidents or mishaps, other participants, automobiles, pedestrians and the like) and death.
-  <br><br>
-                        D. Participant grants to Company and Company’s assigns the irrevocable, sub-licensable right and authority to use Participant’s name, likeness, photograph and/or picture for any and all commercial or non-commercial purposes now known or later developed in perpetuity throughout the universe without further obligation or compensation to Participant.
-  <br><br>
-                        E. The participation in the tours is a voluntary decision taken at participant’s sole risk. Even though the Tours will take all necessary provisions to ensure the safety and wellbeing of all participants, the Tours cannot be held responsible for injuries, loss or damages to participants caused by third parties or due to participant’s own negligence.
-  <br><br>
-                        Participant understands and agrees that this Release will have the effect of releasing, discharging, waiving and forever relinquishing any and all actions or causes of action that he or she may have or have had, whether past, present or future, whether known, or unknown, and whether anticipated or unanticipated by participant, arising out of the participant attending the food tasting and cultural walking tour and/or participating in the activities offered by the Tours.
-  <br><br>
-                        Participant furthermore release and hold harmless the Tours against any and all claims, damages and expenses including but not limited to attorney’s fees arising from other participants of the Tours and/or third parties due to participant’s conduct or acts during the walking tour.
-  <br><br>
-                        F. International Users. The Site is controlled, operated and administered from within the United States. The Company and the Site make no representation that materials, Content, or Chat Content available through the Site are appropriate or available for use outside the United States and access to them from territories where their contents are illegal is prohibited. You may not use the Site or export the Content or Chat Content in violation of U.S. export laws and regulations. If you access the Site from a location outside the United States, you are responsible for compliance with all applicable laws.
-  <br><br>
-                        G. This Waiver and Release of Claims constitutes the entire agreement and understanding between Participant and the Released Parties, and cancels, terminates and supersedes any prior agreement or understanding relating to the subject matter hereof. There are no representations, promises, agreements, warranties, covenants, or undertakings other than those contained in this Waiver and Release of Claims.
-  <br><br>
-                        H. None of the provisions of this Waiver and Release of Claims can be waived or modified except expressly in writing signed by Participant and the party against whom the waiver or modification is sought to be enforced. Failure of any of the Released Parties to enforce any of their rights hereunder at any time shall not act as a waiver to enforce their rights under this Waiver and Release for same or similar acts at any subsequent time. This Waiver and Release of Claims shall be construed broadly to provide a release and waiver to the maximum extent permissible under applicable law.
-  <br><br>
-                        THIS IS A WAIVER AND RELEASE OF LIABILITY. I HAVE READ THIS ENTIRE DOCUMENT. I UNDERSTAND THAT I HAVE GIVEN UP SUBSTANTIAL RIGHTS BY AGREEING TO ITS PROVISIONS. BY CONTINUING TO PARTICIPATE IN THE TOURS, I AM INDICATING MY VOLUNTARY AGREEMENT TO THE PROVISIONS OF THIS WAIVER AND RELEASE OF LIABILITY. PARTICIPATION IN THE TOURS BY A PERSON WHO IS NOT YET 21 YEARS OLD INDICATES THAT A PARENT OR LEGAL GUARDIAN HAS ALSO READ AND CONSENTED TO THIS WAIVER.
-  <br><br>
-                      </p>
-                      
-                      <div class="col-md-12 text-center">
-                      <div class="row">
-                      	<div class="col-md-8"></div>
-                        <div class="col-md-4">
-                        
-                       
-                      <img height="130" src="/assets/foodtour/digital-seal-VERTIKAL-TRIP-LLC.jpg">
-                        </div>
-                      </div>
-                      
-                      </div>
-                      
-                    </div>
-    </div> 
-  </div>
-                    
-					
-					
-					
-					
-				<div style="height:45px;"></div>		
+					<div class="sc-2uw1sw-6 jCDvDq">
+						<h2 class="sc-2uw1sw-7 jIAFpj">Indonesia</h2>
+					</div>
+					<a class="sc-2uw1sw-8 cSIgnb" href="/online-sales/product-list/19305"><span class="sc-jlyJG cGNcCm">Indonesia</span></a>
 				</div>
 			</div>
-        </div>
+		</div>
 	</div>
 </div>
+	
+       		 
 </section>
-
-
-
 @endsection
