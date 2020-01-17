5f2ae4da4075456844118a8b374b83ec9a787375
Budi <aku@budi.my.id>
2020-01-17 23:36:14 +0700

    #1891

--- resources/views/blog/frontend/product.blade.php
+++ resources/views/blog/frontend/product.blade.php
@@ -85,7 +85,7 @@
     <div class="col-sm-4">
     	<div style="height:64px;"></div>
     	<div class="card mb-4 shadow p-2">
-  			<div class="card-header text-white bg-danger"><h5>Book Now</h5></div>
+  			<div class="card-header text-white" style="background-color: #2c97de;"><h5>Book Now</h5></div>
  				 <div class="card-body" style="padding-left:0px;padding-right:0px;padding-top:5px;padding-bottom:15px;">
     				{!! $calendar !!}
   				</div>
