d8d8f23e273379dcae314d9ff354fa927d937fe1
Budi <aku@budi.my.id>
2020-01-18 23:36:51 +0700

    #1891

--- resources/views/blog/frontend/product.blade.php
+++ resources/views/blog/frontend/product.blade.php
@@ -19,6 +19,85 @@
 		
 		@if(str_ireplace("www.","",$_SERVER['HTTP_HOST'])=="budi.my.id" || str_ireplace("www.","",$_SERVER['HTTP_HOST'])=="vertikaltrip.com")
 
+
+		<button class="navbar-toggler navbar-toggler-right  border-dark" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse stroke" id="navbarResponsive">
+			<ul class="navbar-nav text-uppercase ml-auto">
+				<li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                  Indonesia <span class="caret"></span>
+                </a>
+
+                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="nav-link js-scroll-trigger text-dark ml-2" href="/product-list/jogja-food-tour/">Yogyakarta</a>
+                 </div>
+                </li>
+            	
+            	<li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                  Japan <span class="caret"></span>
+                </a>
+
+                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="nav-link js-scroll-trigger text-dark ml-2" href="/product-list/ninja-food-tours/">Shinjuku</a>
+                 </div>
+                </li>
+            	
+                <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                  France <span class="caret"></span>
+                </a>
+
+                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                 <a class="nav-link js-scroll-trigger text-dark ml-2" href="/product-list/original-food-tours-paris/">Paris</a>
+                                  <a class="nav-link js-scroll-trigger text-dark ml-2" href="/product-list/original-food-tours-paris/">Bordeaux</a>
+                                   <a class="nav-link js-scroll-trigger text-dark ml-2" href="/product-list/original-food-tours-paris/">Lyon</a>
+                                    <a class="nav-link js-scroll-trigger text-dark ml-2" href="/product-list/original-food-tours-paris/">Nice</a>
+                 </div>
+                </li>
+               
+                
+				 <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                  Mexico <span class="caret"></span>
+                </a>
+
+                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="nav-link js-scroll-trigger text-dark ml-2" href="/product-list/cancun-food-tours/">Cancun</a>
+                 </div>
+                </li>
+				
+			
+				
+				 <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                  Trinidad and Tobago <span class="caret"></span>
+                </a>
+
+                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="nav-link js-scroll-trigger text-dark ml-2" href="/product-list/trinidad-food-tours/">Trinidad</a>
+                 </div>
+                </li>
+
+                 <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                  India <span class="caret"></span>
+                </a>
+
+                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="nav-link js-scroll-trigger text-dark ml-2" href="/product-list/india-food-tours/">Agra</a>
+                        <a class="nav-link js-scroll-trigger text-dark ml-2" href="/product-list/india-food-tours/">Delhi</a>
+                 </div>
+                </li>
+
+				
+
+				
+			</ul>
+		</div>
+
 		<!-- button class="navbar-toggler navbar-toggler-right border-dark" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="fa fa-search text-white"></span>
 		</button>
@@ -122,8 +201,7 @@
 
 @endif
 
-@if(str_ireplace("www.","",$_SERVER['HTTP_HOST'])=="aaa.com")
-
+@if(str_ireplace("www.","",$_SERVER['HTTP_HOST'])=="budi.my.id" || str_ireplace("www.","",$_SERVER['HTTP_HOST'])=="vertikaltrip.com")
 <section id="booking" style="background-color:#ffffff">
 <div class="container">
   <div class="row" style="padding-bottom:0px;">
@@ -137,18 +215,18 @@
 
   <div class="row">
   	
-    <div class="col-sm-6 col-sm-auto  mb-4">
+    <div class="col-sm-4 col-sm-auto  mb-4">
     	<a href="/product-list/ninja-food-tours">
-    	<div class="card mb-6 shadow p-4 card-block d-table-cell align-middle" style="height:200px;">
-  				 <img class="card-img-top" src="/assets/foodtour/partner/ninja-food.png" alt="Ninja Food Tours">
+    	<div class="card mb-4 shadow p-4 card-block d-table-cell align-middle">
+  				 <img class="card-img-top" src="/assets/foodtour/supplier/ninja-food.png" alt="Ninja Food Tours">
 		</div>
 		</a>
     </div>
 
-    <div class="col-sm-6 col-sm-auto  mb-4">
+    <div class="col-sm-4 col-sm-auto  mb-4">
     	<a href="/product-list/original-food-tours-paris">
-    	<div class="card mb-6 shadow p-4 card-block d-table-cell align-middle" style="height:200px;">
-  				 <img class="card-img-top" src="/assets/foodtour/partner/original-food-tours____.jpg" alt="Original Food Tours">
+    	<div class="card mb-4 shadow p-4 card-block d-table-cell align-middle">
+  				 <img class="card-img-top" src="/assets/foodtour/supplier/original-food-tours____.jpg" alt="Original Food Tours">
 			</div>
 		</a>
     </div>
@@ -156,7 +234,7 @@
     <div class="col-sm-4 col-sm-auto  mb-4">
     		<a href="/product-list/cancun-food-tours">
 			<div class="card mb-4 shadow p-4 card-block d-table-cell align-middle" style="height:200px;">
-  				 <img class="card-img-top" src="/assets/foodtour/partner/cancun-food-tours.png" alt="Cancun Food Tours">
+  				 <img class="card-img-top" src="/assets/foodtour/supplier/cancun-food-tours.png" alt="Cancun Food Tours">
 			</div>
 			</a>
     </div>
@@ -164,7 +242,7 @@
     <div class="col-sm-4 col-sm-auto mb-4">
     	<a href="/product-list/india-food-tours">
     	<div class="card mb-4 shadow p-4 card-block d-table-cell align-middle" style="height:200px;">
-  				 <img class="card-img-top" src="/assets/foodtour/partner/india-food-tours.jpg" alt="India Food Tours">
+  				 <img class="card-img-top" src="/assets/foodtour/supplier/india-food-tours.jpg" alt="India Food Tours">
 		</div>
 		</a>
     </div>
@@ -172,7 +250,15 @@
     <div class="col-sm-4 col-sm-auto mb-4">
     	<a href="/product-list/trinidad-food-tours">
     	<div class="card mb-4 shadow p-4 card-block d-table-cell align-middle" style="height:200px;">
-  				 <img class="card-img-top" src="/assets/foodtour/partner/trinidad.jpg" alt="Trinidad Food Tours">
+  				 <img class="card-img-top" src="/assets/foodtour/supplier/trinidad.jpg" alt="Trinidad Food Tours">
+		</div>
+		</a>
+    </div>
+
+    <div class="col-sm-4 col-sm-auto mb-4">
+    	<a href="/product-list/jogja-food-tour">
+    	<div class="card mb-4 shadow p-4 card-block d-table-cell align-middle" style="height:200px;">
+  				 <img class="card-img-top" src="/assets/foodtour/supplier/jogja.png" alt="Jogja Food Tour">
 		</div>
 		</a>
     </div>
