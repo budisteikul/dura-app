972c5ef930ba330fecc9dfc135ddc04c8cedc9f6
Budi <aku@budi.my.id>
2020-01-20 09:54:20 +0700

    #1891

--- resources/views/blog/frontend/foodtour_shinjuku.blade.php
+++ resources/views/blog/frontend/foodtour_shinjuku.blade.php
@@ -34,7 +34,7 @@
     <div class="col-sm-4">
     	<div style="height:64px;"></div>
     	<div class="card mb-4 shadow p-2">
-  			<div class="card-header text-white" style="background-color: #2c97de;"><h5>Book {{ $title }} Now</h5></div>
+  			<div class="card-header text-white" style="background-color: #2c97de;"><h5>Book {{ $title }}</h5></div>
  			<div class="card-body" style="padding-left:0px;padding-right:0px;padding-top:5px;padding-bottom:15px;">
     				{!!$calendar!!}
 			</div>
@@ -46,15 +46,7 @@
     </div>
 </section>
 
-
-
-
-
-
-
-
-
- <!-- Post Content -->
+<!-- Post Content -->
 <article id="explorer">
 <div class="container">
 	<div class="row">
