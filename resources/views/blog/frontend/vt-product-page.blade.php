7511ac35e2e3c56a2013192e35e7fd3eb02bb136
Budi <aku@budi.my.id>
2020-01-26 00:42:13 +0700

    #1891

--- resources/views/blog/frontend/vt-product-page.blade.php
+++ resources/views/blog/frontend/vt-product-page.blade.php
@@ -31,7 +31,7 @@
     	<div style="height:66px;"></div>
 
 @if($contents->keyPhoto!="")
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -53,11 +53,11 @@
     
 
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
