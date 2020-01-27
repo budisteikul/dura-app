f4f9d905aaa18b1aae23132697b729304b9906a7
Budi <aku@budi.my.id>
2020-01-27 16:04:11 +0700

    #1891

--- resources/views/blog/frontend/booking.blade.php
+++ resources/views/blog/frontend/booking.blade.php
@@ -83,12 +83,18 @@ div {
 		<div class="col-lg-12 col-md-12 mx-auto">
 			<div class="row" style="padding-bottom:0px;">
 				<div class="col-lg-12 text-center">
-				<div style="height:70px;"></div>	
-           
+				<div style="height:70px;"></div>
+				
+           <div class="card mb-8 shadow p-2">
+  			
+ 				 <div class="card-body" style="padding-left:10px;padding-right:10px;padding-top:10px;padding-bottom:15px;">
+		   
 				{!! $product !!}
 				
-					
-				<div style="height:10px;"></div>		
+			</div></div>
+
+			
+				<div style="height:40px;"></div>		
 				</div>
 			</div>
         </div>
