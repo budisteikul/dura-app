917cf92c9e9d605d158b42bb6b04297173630bfe
Budi <aku@budi.my.id>
2020-01-29 11:55:54 +0700

    #1891

--- resources/views/blog/frontend/foodtour.blade.php
+++ resources/views/blog/frontend/foodtour.blade.php
@@ -86,7 +86,7 @@
     });
 </script>
 @else
-<a class="btn btn-danger text-white " href="https://www.vertikaltrip.com/booking/yogyakarta-night-walking-and-food-tours/"><i class="fa fa-ticket-alt"></i> <span style="font-family: 'Barlow','Helvetica Neue',Arial,sans-serif;"><strong>Book now</strong></span></a>	
+<a class="btn btn-danger text-white " href="/booking/yogyakarta-night-walking-and-food-tours/"><i class="fa fa-ticket-alt"></i> <span style="font-family: 'Barlow','Helvetica Neue',Arial,sans-serif;"><strong>Book now</strong></span></a>	
 
 @endif
 
