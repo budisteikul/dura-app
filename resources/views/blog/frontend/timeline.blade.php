221d90e4ea404e00e4615bf54f2c7785109f501b
budi <budi@budis-iMac.local>
2020-02-09 15:07:16 +0700

    #1891

--- resources/views/blog/frontend/timeline.blade.php
+++ resources/views/blog/frontend/timeline.blade.php
@@ -46,12 +46,12 @@
   </nav>
    
 <!-- ################################################################### -->
-	<header id="page-top" class="intro-header" style="background-image: url('{{ $setting->header }}'); background-color:#000000">
+	<header id="page-top" class="intro-header" style="background-image: url('https://res.cloudinary.com/budi/image/upload/v1581235320/eca1ca75-9e80-493f-bfef-cbeb44f8aac3/images/header/DHwfJfutCkQi4hkxbsr7MbHWhd76K0IsNjhyqhiB.jpg'); background-color:#000000">
     	
         <div class="col-lg-8 col-md-10 mx-auto">
     	<div class="site-heading text-center">
         	<div class="transbox" style="min-height:100px; padding-top:25px; padding-bottom:25px; padding-left:10px; padding-right:10px;">
-				<img class="img-circle" src="{{ $setting->gravatar }}">
+				<img class="img-circle" src="https://res.cloudinary.com/budi/image/upload/v1581235316/eca1ca75-9e80-493f-bfef-cbeb44f8aac3/images/gravatar/aaa1.jpg">
 				<hr style="max-width:50px;border-color: #c03b44;border-width: 3px;">
 				<p class="text-faded">
 					{!! nl2br($setting->description) !!}
