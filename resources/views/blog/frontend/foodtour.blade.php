f040d3f3b0e16dbdeb56a19fe8e623c2a30786c6
Budi <aku@budi.my.id>
2020-02-24 10:38:01 +0700

    #1891

--- resources/views/blog/frontend/foodtour.blade.php
+++ resources/views/blog/frontend/foodtour.blade.php
@@ -118,7 +118,7 @@
 <nav class="navbar navbar-default navbar-expand-lg navbar-dark fixed-top shadow mb-5" id="mainNav">
 	<div class="container">
 
-@if(str_ireplace("www.","",$_SERVER['HTTP_HOST'])=="jogjafoodtour.com")
+@if(str_ireplace("www.","",$_SERVER['HTTP_HOST'])=="aaa.com")
 <noscript><a href="https://jogjafoodtour.eventbrite.com" rel="noopener noreferrer" target="_blank"></noscript>
 <button class="btn btn-danger text-white" id="eventbrite-widget-modal-trigger-77732854059" type="button" style="margin-top:10px;margin-bottom:10px;" ><i class="fa fa-ticket-alt"></i> <span style="font-family: 'Barlow','Helvetica Neue',Arial,sans-serif;"><strong>Book now</strong></span></button>
 <noscript></a>Book now on Eventbrite</noscript>
