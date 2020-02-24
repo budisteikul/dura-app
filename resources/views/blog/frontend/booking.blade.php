655be3f888e7f285ef6b123d61b1f14d3a5fa826
Budi <aku@budi.my.id>
2020-02-24 09:37:32 +0700

    #1891

--- resources/views/blog/frontend/booking.blade.php
+++ resources/views/blog/frontend/booking.blade.php
@@ -13,7 +13,33 @@
 @if(str_ireplace("www.","",$_SERVER['HTTP_HOST'])=="jogjafoodtour.com")
 <nav class="navbar navbar-default navbar-expand-lg navbar-dark fixed-top shadow mb-5" id="mainNav-back">
 	<div class="container">
-		<a href="/"><img src="/assets/logo/logo-jogja-istimewa.webp" alt="VERTIKAL TRIP LLC" height="50"  style="margin-top:9px;margin-bottom:9px;"></a>
+		<a href="/"><img src="/assets/logo/logo-jogja-istimewa.webp" alt="JOGJA FOOD TOUR" height="50"  style="margin-top:9px;margin-bottom:9px;"></a>
+		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse stroke" id="navbarResponsive">
+			<ul class="navbar-nav text-uppercase ml-auto">
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#services">Why Jogja Food Tour?</a>
+				</li>
+                
+                <li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#about">The Tour</a>
+				</li>
+                
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#gallery">Snapshot</a>
+				</li>
+                
+                <li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#guide">Tour Guide</a>
+				</li>
+                
+                <li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#review">Reviews</a>
+				</li>
+			</ul>
+		</div>
 	</div>
 </nav>
 <div style="height:25px;"></div>	
