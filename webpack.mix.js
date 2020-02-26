8b2d8bc3cf63e2e0297f03b10d2adb0ade42ee1b
budi <budi@budis-iMac.local>
2020-02-26 20:54:10 +0700

    #1891

--- webpack.mix.js
+++ webpack.mix.js
@@ -57,7 +57,7 @@ mix.scripts([
 	'resources/assets/frontend/wowjs/dist/wow.min.js',
 	'resources/assets/frontend/datatables.net/js/jquery.dataTables.min.js',
 	'resources/assets/frontend/datatables.net-bs4/js/dataTables.bootstrap4.min.js',
-	],'public/js/vertikaltrip-1.1.0.js');
+	],'public/js/vertikaltrip-1.1.1.js');
 
 mix.styles([
 	'resources/assets/frontend/bootstrap-4.3.1/dist/css/bootstrap.min.css',
@@ -71,9 +71,9 @@ mix.styles([
 	'resources/assets/frontend/custom/custom.css',
 	'resources/assets/frontend/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
 	'resources/assets/frontend/datatables.net-bs4/css/dataTables.bootstrap4.css'
-	],'public/css/ratnawahyu.css');
+	],'public/css/ratnawahyu-1.1.1.css');
 	
-mix.styles(['public/css/ratnawahyu.css','resources/assets/frontend/custom/vertikaltrip.css'],'public/css/vertikaltrip-1.1.3.css');
+mix.styles(['public/css/ratnawahyu-1.1.1.css','resources/assets/frontend/custom/vertikaltrip.css'],'public/css/vertikaltrip-1.1.1.css');
 
 mix.copyDirectory('resources/assets/frontend/fontawesome-free-5.9.0-web/webfonts', 'public/webfonts');
 mix.copyDirectory('resources/assets/frontend/back-to-top/img', 'public/img');
