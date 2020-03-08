88c44df24ec71f1328b4735cdd6aab55a9cfc6eb
budi <budi@budis-iMac.local>
2020-03-08 16:49:13 +0700

    #1891

--- resources/views/layouts/admin-lte/blank.blade.php
+++ resources/views/layouts/admin-lte/blank.blade.php
@@ -14,8 +14,8 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <script src="{{asset('js/admin-lte.js')}}"></script>
-  <link rel="stylesheet" href="{{asset('css/admin-lte.css')}}">
+  <script src="{{asset('js/admin-lte3.js')}}"></script>
+  <link rel="stylesheet" href="{{asset('css/admin-lte3.css')}}">
   
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
