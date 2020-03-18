3089e937576e103243d3e5cdfa3c2f103f4cbc1c
Budi <aku@budi.my.id>
2020-03-18 21:13:18 +0700

    #1891

--- resources/views/layouts/vt-menu.blade.php
+++ resources/views/layouts/vt-menu.blade.php
@@ -1,15 +1,12 @@
 		<div class="collapse navbar-collapse stroke" id="navbarResponsive">
 			<ul class="navbar-nav text-uppercase ml-auto">
 				<li class="nav-item">
-					<a class="nav-link js-scroll-trigger" href="/#services">Why Choose us?</a>
+					<a class="nav-link js-scroll-trigger" href="/">Home</a>
 				</li>
                 
                 <li class="nav-item">
-					<a class="nav-link js-scroll-trigger" href="/#tour">The Tour</a>
+					<a class="nav-link js-scroll-trigger" href="/tours">Tours</a>
 				</li>
-                
-                <li class="nav-item">
-					<a class="nav-link js-scroll-trigger" href="/#review">Reviews</a>
-				</li>
+               
 			</ul>
 		</div>
