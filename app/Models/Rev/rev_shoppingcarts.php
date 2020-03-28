5068aad1bca270162c92f8ee29da5aa8ddd689ba
budi <budi@budis-iMac.local>
2020-03-28 12:45:43 +0700

    #1891

--- app/Models/Rev/rev_shoppingcarts.php
+++ app/Models/Rev/rev_shoppingcarts.php
@@ -3,13 +3,11 @@
 namespace App\Models\Rev;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Traits\Uuid;
 
 class rev_shoppingcarts extends Model
 {
     use Uuid;
-	use SoftDeletes;
 	
 	protected $table = 'rev_shoppingcarts';
 	public $incrementing = false;
