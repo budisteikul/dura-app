0e1354871361e1a4545796bf38d8a4dedd3083c9
Budi <aku@budi.my.id>
2020-03-29 09:41:04 +0700

    #1891

--- resources/views/page/terms-and-conditions.blade.php
+++ resources/views/page/terms-and-conditions.blade.php
@@ -1,15 +1,48 @@
 @extends('layouts.frontend')
 @section('content')
+@include('layouts.loading')
 @push('scripts')
-
-
 @endpush
 
-@php
-    $thedomain = "VERTIKAL TRIP";
-@endphp
+
+
+<!-- ################################################################### -->
 
 <!-- Navigation -->
+@if(str_ireplace("www.","",$_SERVER['HTTP_HOST'])=="jogjafoodtour.com")
+<nav class="navbar navbar-default navbar-expand-lg navbar-dark fixed-top shadow mb-5" id="mainNav-back">
+	<div class="container">
+		<a href="/"><img src="/assets/logo/jogjafoodtour.webp" alt="JOGJA FOOD TOUR" height="50"  style="margin-top:9px;margin-bottom:9px;"></a>
+		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse stroke" id="navbarResponsive">
+			<ul class="navbar-nav text-uppercase ml-auto">
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="/#services">Why Jogja Food Tour?</a>
+				</li>
+                
+                <li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="/#about">The Tour</a>
+				</li>
+                
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="/#gallery">Snapshot</a>
+				</li>
+                
+                <li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="/#guide">Tour Guide</a>
+				</li>
+                
+                <li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="/#review">Reviews</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<div style="height:25px;"></div>	
+@else
 <nav class="navbar navbar-default navbar-expand-lg navbar-dark fixed-top shadow mb-5" id="mainNav-back">
 	<div class="container">
 
@@ -23,29 +56,43 @@
 	</div>
 </nav>
 <div style="height:25px;"></div>
-
+@endif
 
 <section id="booking" style="background-color:#ffffff">
 <div class="container">
 	<div class="row">
 		<div class="col-lg-12 col-md-12 mx-auto">
 			<div class="row" style="padding-bottom:0px;">
-				<div class="col-lg-12 text-center">
-				<div style="height:70px;"></div>	
-        
-
-                    
+				<div class="col-lg-12 text-left">
+				<div style="height:70px;"></div>
+				
+           <div class="card mb-8 shadow p-2">
+  			
+ 				 <div class="card-body" style="padding-left:10px;padding-right:10px;padding-top:10px;padding-bottom:15px;">
+                 <div class="text-right">
+		   		
+				 </div>
+                 
+				 
+				 <!-- ##### -->
 					<div class="row" style="padding-bottom:0px;">
 						<div class="col-lg-12 text-center">
-							<!-- h3 class="section-heading" style="margin-top:0px;">Payment method</h3 -->
-							<h4 class="section-subheading text-muted">TERMS AND CONDITIONS</h4>
+							<h3 class="section-heading">TERMS AND CONDITIONS</h3>
 							<hr style="max-width:50px;border-color:#e2433b;border-width:3px;">
+							<h4 class="section-subheading text-muted">
+								
+							</h4>
 						</div>
 					</div>
 					
-                    <div class="row col-md-8  mx-auto text-left">
+                 <!-- ##### -->
+                 <div class="row col-md-8  mx-auto text-left">
 					<div class="textwidget" style=" min-height:250px;">
-                    <p>&nbsp;</p>
+					 <!-- ##### content -->
+					 @php
+						$thedomain = "VERTIKAL TRIP";
+					 @endphp
+						<p>&nbsp;</p>
 <p>These terms and conditions contain legal obligations and we encourage you to read through them carefully. Unless otherwise stated, any purchases made through {{ $thedomain }} are subject to these terms and conditions.</p>
 <p>&nbsp;</p>
 <p><strong>1. OUR CONTRACT</strong></p>
@@ -95,25 +142,25 @@
 <p>{{ $thedomain }} may occasionally update its terms and conditions. When this is done, we will also revise the &lsquo;last updated&rsquo; date of its terms and conditions.</p>
 <p>&nbsp;</p>
 <p class="text-right"><strong>Last updated 22th March 2020<br>
-{{ $thedomain }}</strong></p>							
-	
-                    </div>
-    </div> 
-  </div>
-                    
-                    
-					
-					
-					
-					
-				<div style="height:45px;"></div>		
+{{ $thedomain }}</strong></p>
+					 <!-- ##### -->
+					</div>
+				</div>
+                 <!-- ##### --> 
+                 
+              
+				
+			</div>
+			</div>
+
+			
+				<div style="height:40px;"></div>		
 				</div>
 			</div>
         </div>
 	</div>
 </div>
 </section>
-
 
 
 @endsection
