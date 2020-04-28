b76e0e302ccb07f277ba27c48b27f5d36350d85c
Budi <aku@budi.my.id>
2020-04-28 13:26:36 +0700

    #1891

--- resources/views/blog/frontend/jogjafoodtour.blade.php
+++ resources/views/blog/frontend/jogjafoodtour.blade.php
@@ -140,7 +140,7 @@
     });
 </script>
 @else
-<a class="btn btn-danger text-white " href="https://foodtours.xyz/booking/yogyakarta-night-walking-and-food-tours/" style="margin-top:10px;margin-bottom:10px;" ><i class="fa fa-ticket-alt"></i> <span style="font-family: 'Barlow','Helvetica Neue',Arial,sans-serif;"><strong>Book now</strong></span></a>	
+<a class="btn btn-danger text-white " href="/booking/yogyakarta-night-walking-and-food-tours/" style="margin-top:10px;margin-bottom:10px;" ><i class="fa fa-ticket-alt"></i> <span style="font-family: 'Barlow','Helvetica Neue',Arial,sans-serif;"><strong>Book now</strong></span></a>	
 
 @endif
 
