310ea6f9ea7141c3b7a701189a45d1150a087e65
budi <budi@budis-iMac.local>
2020-05-03 22:18:34 +0700

    #1891

--- resources/views/layouts/all-menu.blade.php
+++ resources/views/layouts/all-menu.blade.php
@@ -1,4 +1,14 @@
 		
+        @if(str_ireplace("www.","",$_SERVER['HTTP_HOST'])=="vertikaltrip.com")
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span> <span style="font-size:13px; color:#FFFFFF">TOURS</span>
+        </button>
+        @else
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span> <span style="font-size:13px; color:#FFFFFF">DESTINATION</span>
+        </button>
+        @endif
+        
         <div class="collapse navbar-collapse stroke" id="navbarResponsive">
 			<ul class="navbar-nav text-uppercase ml-auto">
                 @php
