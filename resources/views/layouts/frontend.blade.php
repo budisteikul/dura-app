20b1fd956297f3d8b5e3b3ac9a15ecb23d740ecf
budi <budi@budis-iMac.local>
2020-05-12 12:32:04 +0700

    #1891

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -17,7 +17,6 @@
 	<meta name="theme-color" content="#2e3237">
     <title>@hasSection('title')@yield('title')@else @if(str_ireplace("www.","",$_SERVER['HTTP_HOST'])=="jogjafoodtour.com")Yogyakarta Night Walking and Food Tours @else Book Amazing Things to Do With VERTIKAL TRIP @endif @endif</title>
     <link href="https://fonts.googleapis.com/css?family=Barlow:400,700" rel="stylesheet" type="text/css" media="screen,handheld">
-	<link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css' media="screen,handheld">
 	<script src="/js/vertikaltrip-3.0.3.js"></script>
 	<link href="/css/vertikaltrip-3.0.3.css" rel="stylesheet" media="screen,handheld">
     @if(env('BOKUN_WIDGET')=='new')
