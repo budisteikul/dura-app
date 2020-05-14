17c39523692cc8286d179068423c197a712ddf20
budi <budi@budis-iMac.local>
2020-05-14 21:20:51 +0700

    #1891

--- resources/views/components/vertikaltrip/booking-widget.blade.php
+++ resources/views/components/vertikaltrip/booking-widget.blade.php
@@ -1,4 +1,4 @@
-   
+  
 <script src="/assets/javascripts/widgets/687035c46b475965b2131d0e804b858e-widget-utils.js" type="text/javascript"></script>
 <link rel="stylesheet" media="screen" href="/assets/stylesheets/32c9c6fd0c7902a484471023a898dec1-activity-time-selector.css">
 <div class="widget-body" id="WidgetContent">
