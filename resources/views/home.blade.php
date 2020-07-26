5e06c181e7503564bb92646531a50ccde2b52639
budi <budi@budis-iMac.local>
2020-07-26 16:52:48 +0700

    #1891

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,7 +35,8 @@ function TOGGLE()
                 <div class="card-header"><i class="fas fa-video"></i> CAMERA</div>
                 <div class="card-body">
                     <div class="embed-responsive embed-responsive-4by3">
-                        <iframe class="embed-responsive-item" src=""></iframe>
+                        <iframe class="embed-responsive-item" src="http://{{ $ipcamera }}/picture/1/frame/
+"></iframe>
                     </div>
                 </div>
             </div>
