fcca221ab81c53dcad30485cdf67c0ea97d737b6
budi <budi@budis-iMac.local>
2020-08-03 21:53:13 +0700

    #1891

--- app/Classes/Home/Relay/GPIO.php
+++ app/Classes/Home/Relay/GPIO.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Classes\Home\Relay;
-
+use App\Models\Home\Relay;
+use Storage;
 class GPIO {
 	public static function switch($gpio,$action){
 		$success = true;
@@ -14,6 +15,36 @@ class GPIO {
 		}
 
 		shell_exec('sudo -u www-data python /home/pi/smarthome/gpio/gpio.py '. $gpio.' '. $cmdn);
+		$phpopen = '<?php 
+		';
+
+		$phpfile = '';
+
+		$relays = Relay::where('type','gpio')->get();
+		foreach($relays as $relay)
+		{
+			
+			$phpfile .= '$file = file_get_contents(\'/home/pi/smarthome/storage/app/relay/'. $relay->id .'\', true);
+					if($file=="off")
+					{
+							print("off");
+							shell_exec(\'sudo -u www-data python /home/pi/smarthome/gpio/gpio.py '. $relay->ipOrGpio .' low\');
+					}
+					else
+					{
+							print("on");
+							shell_exec(\'sudo -u www-data python /home/pi/smarthome/gpio/gpio.py '. $relay->ipOrGpio .' high\');
+					}
+					';
+		}
+		
+
+		$phpclose = ' 
+		?>';
+
+		$phpfile = $phpopen . $phpfile . $phpclose;
+
+		Storage::put('last_state.php', $phpfile);
 		return $success;
 	}
 }
